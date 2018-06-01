9b28a91c738af9a02b7eeaaa387a25554ae40bac
Lillian Zhang <lilyzhangyun@gmail.com>
2018-06-01 18:24:47 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,11 +2,11 @@ def prime?(n)
   if n < 2  
     return false
   end    
-  (2...n).to_a.each { |num| 
+  (2...n).to_a.each do |num| 
     if n % num == 0
       return false
     end
-  }
+  end
   else 
     return true 
 end
